efd9abe0679a101c191c22457985ad569960a251
Hamza-Alqa'abreh <hamzaiqa9@gmail.com>
2022-02-10 19:11:03 +0200

    task finished

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'providers/homeProvider/activitiesAndCources/activities_and_cources_provi
 import 'providers/loginProvider/login_provider.dart';
 import 'ui/pages/home_page.dart';
 import 'ui/pages/login_page.dart';
-
 import 'providers/homeProvider/categories/categories_provider.dart';
+
 
 
 void main() async {
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             title: 'Task app',
             home: Consumer<LoginProvider>(
-              builder: (context, loginProvider, _) => loginProvider.isLoggedIn ? HomePage() : LoginPage(),
+              builder: (context, loginProvider, _) => loginProvider.isLoggedIn ? const HomePage() : const LoginPage(),
             ),
           ));
   }
